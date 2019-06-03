d79d5eedbe4fc275223c6e69ec8ab3380ac60db7
JuliaJasinska <julia.jasiska@gmail.com>
2019-06-04 01:11:01 +0200

    letter recognition
    andrea to anioł <3

--- Unshrederator2/Piece.py
+++ Unshrederator2/Piece.py
@@ -2,6 +2,7 @@ from gutenberg.acquire import load_etext
 from gutenberg.cleanup import strip_headers
 from gutenberg.query import list_supported_metadatas, get_metadata
 
+import base64
 
 class Piece:
     def __init__(self):
@@ -12,3 +13,14 @@ class Piece:
 
     def show_piece(self):
         print("This is piece")
+
+    def find_letter(self, scinek):
+        with open(scinek, "rb") as imageFile:
+            stri = base64.b64encode(imageFile.read())
+        assert isinstance(stri, str)
+        return self.find_letter_in_string(stri)
+
+    def find_letter_in_string(self, scinek):
+        return set(scinek)
+
+
